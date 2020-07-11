414f1fc2d48582f5725d2c54a25c364eb4019b30
Gabriel Goonçalves <gabgoncalveso@gmail.com>
2020-07-11 13:08:08 -0300

    4 desafios completos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:calculadora_de_juros/pages/calculator_page.dart';
+import 'package:interest_calculator/pages/calculator_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,22 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp
+      DeviceOrientation.portraitUp,
     ]);
-    return MaterialApp(
-      title: "Calculadora de Juros",
-      theme: ThemeData(primarySwatch: Colors.deepPurple, canvasColor: Colors.deepPurple,),
-      debugShowCheckedModeBanner: false,
-      home: CalculatorPage(),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate, 
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
-      ],
-      supportedLocales: [
-        const Locale('pt', 'BR'),
-        const Locale('en', 'US')
-      ],
+
+    return DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => new ThemeData(
+        primarySwatch: Colors.deepPurple,
+        brightness: brightness,
+      ),
+      themedWidgetBuilder: (context, theme) {
+        return MaterialApp(
+          title: 'Calculadora de Juros',
+          theme: theme,
+          debugShowCheckedModeBanner: false,
+          home: CalculatorPage(),
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [
+            const Locale('pt', 'BR'),
+            const Locale('en', 'US'),
+          ],
+        );
+      }
     );
   }
 }
--- lib/pages/calculator_page.dart
+++ lib/pages/calculator_page.dart
@@ -1,261 +1,310 @@
-  
 import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:interest_calculator/controllers/controller.dart';
+import 'package:interest_calculator/helpers/constantes.dart';
+import 'package:interest_calculator/models/result_payment_slip.dart';
 import 'package:intl/intl.dart';
-import 'package:calculadora_de_juros/controllers/controller.dart';
-import 'package:calculadora_de_juros/helpers/constantes.dart';
-import 'package:calculadora_de_juros/models/result_payment_slip.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 
 class CalculatorPage extends StatefulWidget {
   @override
   _CalculatorPageState createState() => _CalculatorPageState();
+  Widget build(BuildContext context) {
+  }
 }
 
 class _CalculatorPageState extends State<CalculatorPage> {
-  final _inputFieldHeight = 60.0;
-  final _formKey = GlobalKey<FormState>();
-  final _dateFormat = DateFormat('dd/MM/yyyy');
-  final _moneyFormat = NumberFormat("#,##0.00", "pt_BR");
-  Controller _controller = Controller();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _buildAppBar(),
-      body: SingleChildScrollView(
-        
-        child:Container(
-          
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [Colors.blue[700], Colors.blue[400]])
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                _buildTitle('Informações do boleto'),
-                _buildMoneyInputField(
-                  'Valor do boleto',
-                  controller: _controller.moneyController,
-                  onSaved: (value){
-                    _controller.paymentSlip.money = _controller.moneyController.numberValue;
-                  },
-                ),
-                _buildDateInputField(
-                  'Data de vencimento',
-                  onSaved: (date){
-                    _controller.paymentSlip.dueDate = date;
-                  },
-                ),
-                _buildDateInputField(
-                  'Data de pagamento',
-                  onSaved: (date){
-                    _controller.paymentSlip.payDate = date;
-                  },
-                ),
-                _buildTitle('Multa'),
-                _buildMoneyInputField(
-                  'Valor da multa',
-                  controller: _controller.feeController,
-                  onSaved: (value){
-                    _controller.paymentSlip.feeValue = _controller.feeController.numberValue;
-                  },
-                ),
-                _buildRateRadioButton(
-                  context,
-                  onChanged: (label){
-                    print(label);
-                    _controller.paymentSlip.feeType = label;
-                  },
-                ),
-                _buildTitle('Juros'),
-                _buildMoneyInputField(
-                  'Valor do juros',
-                  controller: _controller.interestController,
-                  onSaved: (value){
-                    _controller.paymentSlip.interestValue = _controller.interestController.numberValue;
-                  },
-                ),
-                _buildRateRadioButton(
-                  context,
-                  onChanged: (label){
-                    print(label);
-                    _controller.paymentSlip.interestType = label;
-                  },
-                ),
-                _buildPeriodRadioButton(
-                  context,
-                  onChanged: (label){
-                    print(label);
-                    _controller.paymentSlip.interestPeriod = label;
-                  },
-                ),
-                _buildRoundedButton(
-                  context,
-                  label: 'CALCULAR',
-                  padding: EdgeInsets.only(top: 32.0),
-                  onTap: (){
-                    if(_formKey.currentState.validate()){
-                      _formKey.currentState.save();
-                      var result = _controller.calculate();
-                      _showResultDialog(result);
-                    }
-                  },
-                ),
-                _buildRoundedButton(
-                  context,
-                  label: 'LIMPAR',
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  onTap: (){
-                    setState(() {
-                      _controller.clear();
-                    });
-                  }
-                )
-              ],
-            ),
-          ),
-        ),
-      )
-    );
-  }
-
-  _buildAppBar(){
+  
+  _buildAppBar() {
     return AppBar(
-      title: Text("Calculadora de Juros"),
+      title: Text('Calculadora de Juros'),
       actions: <Widget>[
         IconButton(
-          icon: Icon(Icons.lightbulb_outline),
-          onPressed: () {},
+          icon: Theme.of(context).brightness == Brightness.dark
+          ?   Icon(FontAwesomeIcons.lightbulb)
+          : Icon(FontAwesomeIcons.moon),
+          onPressed: () async{
+              _changeBrightness();
+              //_changeColor();
+          },
         ),
         IconButton(
-          icon: Icon(Icons.info), 
-          onPressed: () {},
-        )
+          icon: Icon(Icons.info),
+          onPressed: () {
+            showAlertDialog1(context);
+          },
+        ),
       ],
     );
   }
 
-  _buildTitle(String title, {double top = 16.0, double bottom = 8.0}){
-    return Container(
-      padding: EdgeInsets.fromLTRB(0.0, top, 0.0, bottom),
-      child: Text(
-        title,
-        style: TextStyle(
-          color: Theme.of(context).primaryColor,
-          fontWeight: FontWeight.w500
-        ),
-      ),
-    );
+  void _changeBrightness() {
+    DynamicTheme.of(context).setBrightness(
+      Theme.of(context).brightness == Brightness.dark
+      ? Brightness.light
+      : Brightness.dark);
   }
 
-  _buildMoneyInputField(String label, {MoneyMaskedTextController controller, Function(String) onSaved}){
-    return Container(
-      height: _inputFieldHeight,
-      padding: EdgeInsets.symmetric(vertical: 8.0),
-      child: TextFormField(
-        controller: controller,
-        keyboardType: TextInputType.number,
-        onSaved: onSaved,
-        decoration: InputDecoration(
-          labelText: label,
-          border: _roundedInputDecoration(),
-        ),
-      ),
-    );
-  }
+  showAlertDialog1(BuildContext context) { 
+    // configura o button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {
+       Navigator.of(context).pop();
+     },
+  );
+  // configura o  AlertDialog
+  AlertDialog alerta = AlertDialog(
+    title: Text("Aplicativo Calculadora de Juros"),
+    content: Text("Desenvolvido por: Gabriel Gonçalves de Oliveira.\n\nCurso: Análise e Desenvolvimento de Sistemas \n\nFatec Americana"),
+    actions: [
+      okButton,
+    ],
+  );
+  // exibe o dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alerta;
+    },
+  );
+}
 
-  _roundedInputDecoration(){
+
+  _buildTitle(String title, {double top = 16.0, double bottom = 8.0}) {
+  return Container(
+    padding: EdgeInsets.fromLTRB(0.0, top, 0.0, bottom),
+    child: Text(
+      title,
+      style: TextStyle(
+        color: Theme.of(context).primaryColor,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+  );
+}
+
+  _buildMoneyInputField(String label,
+    {MoneyMaskedTextController controller, Function(String) onSaved}) {
+  return Container(
+    height: _inputFieldHeight,
+    padding: EdgeInsets.symmetric(vertical: 8.0),
+    child: TextFormField(
+      controller: controller,
+      keyboardType: TextInputType.number,
+      onSaved: onSaved,
+      decoration: InputDecoration(
+        labelText: label,
+        border: _roundedInputDecoration(),
+      ),
+    ),
+  );
+}
+
+_roundedInputDecoration() {
     return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(_inputFieldHeight * 0.5)
+      borderRadius: BorderRadius.circular(_inputFieldHeight * 0.5),
     );
-  }
+}
 
-  _buildDateInputField(String label, {Function(DateTime) onSaved}){
-    return Container(
-      height: _inputFieldHeight,
-      padding: EdgeInsets.symmetric(vertical: 8.0),
-      child: DateTimeField(
-        decoration: InputDecoration(
-          labelText: label,
-          border: _roundedInputDecoration(),
-        ),
-        format: _dateFormat,
-        initialValue: DateTime.now(),
-        onSaved: onSaved,
-        onShowPicker: (context, currentValue){
-          return showDatePicker(
-            context: context, 
-            firstDate: DateTime(1900), 
-            initialDate: currentValue ?? DateTime.now(), 
-            lastDate: DateTime(2100)
-          );
-        },
+_buildDateInputField(String label, {Function(DateTime) onSaved}) {
+  return Container(
+    height: _inputFieldHeight,
+    padding: EdgeInsets.symmetric(vertical: 8.0),
+    child: DateTimeField(
+      decoration: InputDecoration(
+        labelText: label,
+        border: _roundedInputDecoration(),
       ),
-    );
-  }
+      format: _dateFormat,
+      initialValue: DateTime.now(),
+      onSaved: onSaved,
+      onShowPicker: (context, currentValue) {
+        return showDatePicker(
+          context: context,
+          firstDate: DateTime(1900),
+          initialDate: currentValue ?? DateTime.now(),
+          lastDate: DateTime(2100),
+        );
+      },
+    ),
+  );
+}
 
-  _buildRateRadioButton(BuildContext context, {Function(dynamic) onChanged}){
-    return CustomRadioButton(
-      enableShape: true,
-      elevation: 0,
-      buttonColor: Theme.of(context).canvasColor,
-      buttonLables: Constantes.rateLabels,
-      buttonValues: Constantes.rateLabels,
-      radioButtonValue: onChanged,
-      selectedColor: Theme.of(context).canvasColor,
-    );
-  }
+_buildRateRadioButton(BuildContext context, {Function(dynamic) onChanged}) {
+  return CustomRadioButton(
+    enableShape: true,
+    elevation: 0,
+    buttonColor: Theme.of(context).canvasColor,
+    buttonLables: Constantes.rateLabels,
+    buttonValues: Constantes.rateLabels,
+    radioButtonValue: onChanged,
+    selectedColor: Theme.of(context).accentColor,
+  );
+}
 
-  _buildPeriodRadioButton(BuildContext context, {Function(dynamic) onChanged}){
-    return CustomRadioButton(
-      enableShape: true,
-      elevation: 0,
-      buttonColor: Theme.of(context).canvasColor,
-      buttonLables: Constantes.periodLabels,
-      buttonValues: Constantes.periodLabels,
-      radioButtonValue: onChanged,
-      selectedColor: Theme.of(context).canvasColor,
-    );
-  }
+_buildPeriodRadioButton(BuildContext context, {Function(dynamic) onChanged}) {
+  return CustomRadioButton(
+    enableShape: true,
+    elevation: 0,
+    buttonColor: Theme.of(context).canvasColor,
+    buttonLables: Constantes.periodLabels,
+    buttonValues: Constantes.periodLabels,
+    radioButtonValue: onChanged,
+    selectedColor: Theme.of(context).accentColor,
+  );
+}
 
-  _buildRoundedButton(BuildContext context, {String label = '', Function onTap, EdgeInsets padding}){
-    return Padding(
-      padding: padding,
-      child: InkWell(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(_inputFieldHeight * 0.5),
-            color: Theme.of(context).canvasColor,
-          ),
-          height: _inputFieldHeight * 0.8,
-          child: Center(
-            child: Text(
-              label,
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w500,
-              ),
+_buildRoundedButton(BuildContext context,
+    {String label = '', Function onTap, EdgeInsets padding}) {
+  return Padding(
+    padding: padding,
+    child: InkWell(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(_inputFieldHeight * 0.5),
+          color: Theme.of(context).primaryColor,
+        ),
+        height: _inputFieldHeight * 0.8,
+        child: Center(
+          child: Text(
+            label,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
             ),
           ),
         ),
-        onTap: onTap,
       ),
-    );
-  }
-  void _showResultDialog(ResultPaymentSlip result){
+      onTap: onTap,
+    ),
+  );
+}
+
+final _inputFieldHeight = 60.0;
+final _formKey = GlobalKey<FormState>();
+final _dateFormat = DateFormat('dd/MM/yyyy');
+final _moneyFormat = NumberFormat("#,##0.00", "pt_BR");
+
+// Lógica de negócio do aplicativo
+Controller _controller = Controller();
+                                                                        
+@override                                                              
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: _buildAppBar(),
+    body: SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.white, Colors.black12])),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildTitle('Informações do boleto'),
+            _buildMoneyInputField(
+              'Valor do boleto',
+              controller: _controller.moneyController,
+              onSaved: (value) {
+                _controller.paymentSlip.money =
+                    _controller.moneyController.numberValue;
+              },
+            ),
+            _buildDateInputField(
+              'Data de vencimento',
+              onSaved: (date) {
+                _controller.paymentSlip.dueDate = date;
+              },
+            ),
+            _buildDateInputField(
+              'Data de pagamento',
+              onSaved: (date) {
+                _controller.paymentSlip.payDate = date;
+              },
+            ),
+            _buildTitle('Multa'),
+            _buildMoneyInputField(
+              'Valor da multa',
+              controller: _controller.feeController,
+              onSaved: (value) {
+                _controller.paymentSlip.feeValue =
+                    _controller.feeController.numberValue;
+              },
+            ),
+            _buildRateRadioButton(
+              context,
+              onChanged: (label) {
+                print(label);
+                _controller.paymentSlip.feeType = label;
+              },
+            ),
+            _buildTitle('Juros'),
+            _buildMoneyInputField(
+              'Valor do juros',
+              controller: _controller.interestController,
+              onSaved: (value) {
+                _controller.paymentSlip.interestValue =
+                    _controller.interestController.numberValue;
+              },
+            ),
+            _buildRateRadioButton(
+              context,
+              onChanged: (label) {
+                print(label);
+                _controller.paymentSlip.interestType = label;
+              },
+            ),
+            _buildPeriodRadioButton(
+              context,
+              onChanged: (label) {
+                print(label);
+                _controller.paymentSlip.interestPeriod = label;
+              },
+            ),
+            _buildRoundedButton(
+              context,
+              label: 'CALCULAR',
+              padding: EdgeInsets.only(top: 32.0),
+              onTap: () {
+                if (_formKey.currentState.validate()) {
+                  _formKey.currentState.save();
+                  var result = _controller.calculate();
+                  _showResultDialog(result);
+                }
+              },
+            ),
+            _buildRoundedButton(
+              context,
+              label: 'LIMPAR',
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              onTap: () {
+                setState(() {
+                  _controller.clear();
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    ),
+  ),
+  );
+}
+
+void _showResultDialog(ResultPaymentSlip result) {
     showDialog(
       context: context,
-      builder: (BuildContext context){
+      builder: (BuildContext context) {
         return AlertDialog(
           title: Text("Resultado"),
           content: Column(
@@ -272,19 +321,19 @@ class _CalculatorPageState extends State<CalculatorPage> {
             ],
           ),
         );
-      }
-
+      },
     );
-
   }
-  _buildResultDialogRow(String label, String value){
+
+  _buildResultDialogRow(String label, String value) {
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Text(label, textAlign: TextAlign.left, style: TextStyle(color: Colors.black),),
-        Text(label, textAlign: TextAlign.right, style: TextStyle(color: Colors.black)),
+        Text(label, textAlign: TextAlign.left),
+        Text(value, textAlign: TextAlign.right),
       ],
     );
   }
+
 }
